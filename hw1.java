8b3acbf21e31fb015b5f4fb92dd6dece28eeeea6
SmolyaninovMikhail <121110707+SmolyaninovMikhail@users.noreply.github.com>
2023-05-24 20:14:13 +0300

    Update hw1.java

--- hw1.java
+++ hw1.java
@@ -3,10 +3,10 @@ public class hw1 {
     final static int MAX = 2000;
 
     public static void main(String[] args) {
-        
+        // Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
         int i = getRandomNumber(MIN, MAX);
         System.out.println("Случайное целое число в диапазоне от " + MIN + " до " + MAX + " = " + i);
-        
+        // Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
         int n = high_bit_line(i);
         System.out.println("Номер старшего значащего бита выпавшего числа: " + n);
                 
